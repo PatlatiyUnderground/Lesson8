226d081b1dd3c0bc5bf7c3d2a0c3cbe3cfac029d
PatlatiyUnderground <patlatiyunderground@gmail.com>
2025-05-24 19:57:25 +0300

    Lesson complete

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        //Написать программу, которая сгенерирует случайный вес 10 людям в пределах от 40 до 100 кг.
+        // (Нужно самостоятельно найти информацию, как генерировать числа в заданном диапазоне).
+        //
+        //Далее вам нужно будет вычислить несколько показателей:
+        //1. Средний вес всех людей.
+        //2. Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно.
+
+        int max = 100;
+        int min = 40;
+        int focus = 0;
+
+       int[] array = new int[10];
+       for (int i = 0; i < array.length; i++) {
+          int r = (int) (Math.random() * (max - min + 1)) +min;
+           array[i] = r;
+           if (array[i] >= 60 && array[i] <= 80) {
+               focus++;
+           }
+       }
+
+       for (int i = 0; i < array.length; i++) {
+           System.out.println(array[i]);
+       }
+
+        System.out.println("\nКолличество людей от 60 до 80 кг равно " + focus);
+
+       int sum = 0;
+       for (int i = 0; i < array.length; i++) {
+           sum += array[i];
+       }
+       System.out.println("\nСредний вес всех людей равен " + sum + "/" + array.length+ "=" + sum/array.length);
+
     }
 }
